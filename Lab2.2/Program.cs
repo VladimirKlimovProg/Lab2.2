e5a063a9c9b0e2bd2d40967af690a8240e1b9086
Vladimir Klimov <vladimirklimoff@yandex.ru>
2018-10-23 04:06:20 +0500

    fix bugs

--- Lab2.2/Program.cs
+++ Lab2.2/Program.cs
@@ -10,6 +10,43 @@ namespace Lab2._2
     {
         static void Main(string[] args)
         {
+            double currentElement = 1;
+            double currentMaxElement = -1.7e+308;
+            int currentNumber = 0;
+            int currentMaxNumber = 0;
+
+            while (currentElement != 0)
+            {
+                Console.WriteLine("Введите текущий элемент. Чтобы завершить последовательность введите 0");
+
+                try
+                {
+                    currentElement = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Некорректный ввод");
+                    return;
+                }
+
+                if (currentElement != 0)
+                {
+                    currentNumber++;
+                    if (currentElement >= currentMaxElement)
+                    {
+                        currentMaxElement = currentElement;
+                        currentMaxNumber = currentNumber;
+                    }
+                }
+            }
+            if (currentNumber == 0)
+            {
+                Console.WriteLine("Нет элементов в последовательности. Вы ввели только 0");
+            }
+            else
+            {
+                Console.WriteLine($"Номер максимального элемента в последовательности: {currentMaxNumber}");
+            }
         }
     }
 }
